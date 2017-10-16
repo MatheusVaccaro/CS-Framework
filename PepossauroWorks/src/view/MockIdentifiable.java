8d90eabfb12442eee9962accd7c643196c89d49f
Max Zorzetti <maxzorzetti@live.com>
2017-10-16 02:33:52 -0200

    Updates MockIdentifiable to match Identifiable

--- PepossauroWorks/src/view/MockIdentifiable.java
+++ PepossauroWorks/src/view/MockIdentifiable.java
@@ -1,10 +1,24 @@
 package view;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import database.Identifiable;
 
 public class MockIdentifiable extends Identifiable {
 	
+	private int mockData;
+	
 	public MockIdentifiable(String id) {
 		this.id = id;
+		this.mockData = id.hashCode() % 100;
+	}
+
+	public Map<String, String> getComplexRepresentation() {
+		Map<String, String> complexRep = new HashMap<String, String>();
+		complexRep.put("id", id.toString());
+		complexRep.put("data", "" + mockData);
+		
+		return complexRep;
 	}
 }
