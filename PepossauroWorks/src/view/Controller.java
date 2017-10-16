8054a39e2603474de6b9141fe4c57f8789733cbd
Matheus Vaccaro <matheusvaccaro@icloud.com>
2017-10-16 00:45:40 -0200

    Adds method to select a cell from the left list

--- PepossauroWorks/src/view/Controller.java
+++ PepossauroWorks/src/view/Controller.java
@@ -1,24 +1,46 @@
 package view;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ListView.EditEvent;
+import javafx.scene.control.cell.TextFieldListCell;
 
 public class Controller {
-	
-    @FXML
-    private ListView<String> listView;
-    
-    public Controller() {}
-    
-    @FXML
-    private void initialize() {
 
-        ObservableList<String> items = FXCollections.observableArrayList (
-            "Single", "Double", "Suite", "Family App");
-        listView.setItems(items);
-        listView.refresh();
-        System.out.println(listView);
-    }
+	@FXML
+	private ListView<String> listView;
+
+	@FXML
+	private ListView<String> rightListView;
+
+	public Controller() {
+	}
+
+	@FXML
+	private void initialize() {
+
+		ObservableList<String> items = FXCollections.observableArrayList("Single", "Double", "Suite", "Family App");
+		listView.setItems(items);
+		listView.refresh();
+		System.out.println(listView);
+
+		listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+			public void changed(ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
+				selectionChanged(ov, oldValue, newValue);
+			}
+		});
+		
+		listView.getSelectionModel().selectFirst();
+
+	}
+
+	private void selectionChanged(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+		// retrieve the selected row data and add it to rightListView
+	}
 }
+
